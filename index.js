99d194d154c2e6130e33459717cb73779588b067
javierdebug <99869456+javierdebug@users.noreply.github.com>
2022-05-25 00:56:08 -0500

    Improve mathematical logic and add new functions

--- index.js
+++ index.js
@@ -15,18 +15,34 @@ function insertResult(result) {
 document.querySelector("#buttons").addEventListener('click', (e) => {
     //console.log(e.target);
     if (e.target.classList[1] === 'btn-num') {
-        arrayValues.push(e.target.innerText*1);
+        //arrayValues.push(e.target.innerText*1);
         insert(e.target.innerText);
     }
-    if (e.target.classList[1] === 'btn-special') {
-        if (typeof(arrayValues[arrayValues.length - 1]) == "number" ) {
-            arrayValues.push(e.target.innerText);
+    if (e.target.classList[1] === 'btn-special' && e.target.classList[2] != 'btn-exp1') {
+        // if (typeof(arrayValues[arrayValues.length - 1]) == "number" ) {
+        //     //arrayValues.push(e.target.innerText);
             insert(e.target.innerText);
-        } else {
-            arrayValues.pop();
-            arrayValues.push(e.target.innerText);
-            insert(e.target.innerText);
-        }
+        // } else {
+        //     //arrayValues.pop();
+        //     //arrayValues.push(e.target.innerText);
+            // insert(e.target.innerText);
+        // }
+    }
+    if (e.target.classList[2] === 'btn-exp1') {
+        insert('^(-1)');        
+    }
+})
+
+const availableChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', '-', '(', ')', '.'];
+window.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    console.log(e.key);
+    if (e.key == "Escape") {document.querySelector("#btn-AC").click();}
+    else if (e.key == "Enter") {document.querySelector("#btn-equal").click();} 
+    else if (e.key == 'Backspace') {document.querySelector("#Del").click();}
+    else if (e.key == '*') {insert('x');}
+    else if (availableChars.indexOf(e.key) > -1) {
+        insert(e.key);
     }
 })
 
@@ -36,28 +52,69 @@ document.querySelector("#btn-AC").addEventListener('click', (e) => {
     strNums = "";
     strFuns = "";
     result = 0;
+    // saveToHistorial();
     document.querySelector("#display").innerHTML = "";
     document.querySelector("#result").innerHTML = "=";
 })
 
-let strNums = "";
-let strFuns = "";
+// Implement backspace button functionality:
+document.querySelector("#Del").addEventListener('click', (e) => { 
+    let content = document.querySelector("#display").textContent;
+    inputVal = content.substring(0,content.length - 1);
+    document.querySelector("#display").innerHTML = "";
+    insert(inputVal);
+})
+
 let expression = "";
 let result = 0;
 document.querySelector("#btn-equal").addEventListener('click', (e) => {
-    //console.log(arrayValues);
-    // strNums = arrayValues.join('').split(/[x+-/]/);
-    // strFuns = arrayValues.join('').split(/\d+/).join('').split('');
-    
-    // console.log(strNums,strFuns);
-    // str = str.split(/\d+/gm);
-    // console.log(str);
-    expression = document.querySelector("#display").innerText;
-    expression = expression.replace(/[^()-/\d\/x+.^]/g, "");
-    expression = expression.replace(/x/g, "*");
-    expression = expression.replace(/\^/g, "**");
-    result = eval(expression);
+    expression = document.querySelector("#display").innerText; //Select the current math expression.
+    // expression = expression.replace(/[^()-/\d\/x+.^]/g, ""); //Replace any text other than digits and math operators.
+    expression = expression.replace(/x/g, "*"); //Replace symbol 'x' with multiplication operator.
+    expression = expression.replace(/\^/g, "**"); //Replace symbol '^' with exponential operator.
+    expression = expression.replace(/cos/g, "Math.cos");
+    expression = expression.replace(/sin/g, "Math.sin");
+    expression = expression.replace(/tan/g, "Math.tan");
+    expression = expression.replace(/π/g, "Math.PI");
+    expression = expression.replace(/log/g, "Math.log10");
+    expression = expression.replace(/ln/g, "Math.log");
+    expression = expression.replace(/e/g, "Math.E");
+    let matchRegex = /(?<=√)[\w+x^()]+/g;
+    let found = expression.match(matchRegex);
+    //console.log(matchRegex, found);
+    expression = expression.replace(/(?<=√)[\w+x^()]+/g, `(${found})`);
+    expression = expression.replace(/√/g, "Math.sqrt");
+    found = expression.match(/\d+(?=\()|\d+(?=M)/g);
+    console.log(found);
+    expression = expression.replace(/\d+(?=\()|\d+(?=M)/g, `(${found})*`);
+    expression = expression.replace(/\)(?=\d+)/g, ")*");
 
+    // expression = expression.replace(/(?<=√\d)/g, "(");
+    // expression = expression.replace(/\d/g, "")
+    console.log(expression);
+
+    calculate(expression); //Eval the math expression.
     insertResult(result);
+    saveToHistorial();
+
 });
 
+function calculate(expression) {
+    try {
+        result = eval(expression);
+        return result
+    }
+    catch(err) {
+        result = "Error";
+        return result
+    } //Catch any found error and display 'Error' message.
+}
+
+function saveToHistorial() {
+    let equation = document.querySelector("#display").innerText;
+    let resultH = document.querySelector("#result").innerText;
+       
+    if (resultH != "=Error" && resultH !="=undefined" && resultH !="=function sqrt() { [native code] }") {
+        document.querySelector("#historial").insertAdjacentHTML("afterbegin", `${equation}${resultH} <br>`);
+    }
+}
