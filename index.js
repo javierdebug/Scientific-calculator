90f2401431bde864cf053c277a88ae7ebd98570d
javierdebug <99869456+javierdebug@users.noreply.github.com>
2022-06-03 23:08:48 -0500

    Improve the math functionality

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 let inputVal = "";
-let firstVal = 0;
-let secondVal = 0;
 let arrayValuesDisplay = [];
 let arrayValuesCalculate = [];
 let ans = 0;
 
-
 function insert(inputVal) {
-    //console.log(inputVal);
-    // if (arrayValuesCalculate[arrayValuesCalculate.length-2] == '*' && arrayValuesCalculate[arrayValuesCalculate.length-1] == '*') {
-    //     //console.log('multi');
-    //     let content = document.querySelector("#display").textContent;
-    //     inputVal = content.substring(0,content.length - 1);
-    //     document.querySelector("#display").innerHTML = "";
-    //     document.querySelector("#display").insertAdjacentHTML("beforeend", `${inputVal}`);
-    //     document.querySelector("#display").insertAdjacentHTML("beforeend", `^`);
-    //     arrayValuesDisplay.pop();
-    //     arrayValuesCalculate.pop();
-    //     arrayValuesCalculate.pop();
-    //     arrayValuesCalculate.push('**');
-    // } else {
-    //     document.querySelector("#display").insertAdjacentHTML("beforeend", `${inputVal}`);
-    // }
+
 }
 
 function insertResult(result) {
@@ -30,30 +13,38 @@ function insertResult(result) {
 }
 
 document.querySelector("#buttons").addEventListener('click', (e) => {
-    //console.log(e.target);
+
+    console.log(e.target.classList[1]);
     if (e.target.classList[1] === 'btn-num') {
-        arrayValuesCalculate.push(e.target.innerText);
-        insert(e.target.innerText);
-        arrayValuesDisplay.push(e.target.innerText);
+        arrayValuesCalculate.push(e.target.innerText*1);
+        arrayValuesDisplay.push(e.target.innerText*1);
     }
+
+    if (e.target.classList[2] === 'btn-sign') {
+        changeSign();
+    }
+    
+    if (e.target.classList[1] === "btn-dot") {
+        arrayValuesCalculate.push('.');
+        arrayValuesDisplay.push('.');
+    }
+
+    //Check if EXP mode activate:
+    checkEXPMode(e);
+
     if (e.target.classList[1] === 'btn-special' && e.target.classList[3] != 'btn-none') {
 
-    insert(e.target.innerText);
     arrayValuesDisplay.push(e.target.innerText);
         
         if (e.target.classList[2] == 'btn-alg') {
             arrayValuesCalculate.push(`Math.${e.target.classList[3]} `);
             arrayValuesCalculate.push('(');
-            insert(`(`);
             arrayValuesDisplay.push('(');
         } else if (e.target.classList[2] == 'btn-mult') {
             arrayValuesCalculate.push('*');
-        // } else if (e.target.classList[2] == 'btn-pow') {
-        //     arrayValuesCalculate.push('**');
         } else if (e.target.classList[2] == 'btn-sqrt') {
             arrayValuesCalculate.push('Math.sqrt ');
             arrayValuesCalculate.push('(');
-            insert('(');
             arrayValuesDisplay.push('(');
         } else if (e.target.classList[2] == 'btn-PI') {
             arrayValuesCalculate.push('Math.PI ');
@@ -66,9 +57,17 @@ document.querySelector("#buttons").addEventListener('click', (e) => {
             arrayValuesCalculate.push(e.target.innerText);
         }
     }
+
+    if (e.target.classList[2] == 'btn-EXP') {
+        arrayValuesCalculate.push('*10** (1* ');
+        arrayValuesDisplay.push("x10<sup>");
+        //modeOnlyDigit(e);
+        modeOnlyDigitKey = 1;
+    }
+
     if (e.target.classList[2] === 'btn-exp1') {
-        arrayValuesCalculate.push('**(-1');
-        arrayValuesCalculate.push(')');
+        arrayValuesCalculate.push('**(-1)');
+        // arrayValuesCalculate.push(')');
         insert('⁻¹');        
         arrayValuesDisplay.push('⁻¹');        
         // insert('<sup>-1</sup>');
@@ -90,25 +89,43 @@ document.querySelector("#buttons").addEventListener('click', (e) => {
         insert('^');
         arrayValuesDisplay.push('^');
     }
-    if (e.target.classList[2] === 'btn-sign') {
-        changeSign();
+    if (e.target.classList[2] === 'btn-Ans') {
+        arrayValuesCalculate.push(ans);
+        arrayValuesDisplay.push('Ans');
     }
+
     // if (e.target.classList[2] === 'btn-fact') {
     //     arrayValuesCalculate.push('!');
     //     insert('!')
     //}
-    if (e.target.classList[2] === 'btn-abs') {
-        if (arrayValuesCalculate.length == 0) {
-            arrayValuesCalculate.push('Math.abs ( ');
-        } else if (arrayValuesCalculate.length - 1) {
+    // if (e.target.classList[2] === 'btn-abs') {
+    //     if (arrayValuesCalculate.length == 0) {
+    //         arrayValuesCalculate.push('Math.abs ( ');
+    //     } else if (arrayValuesCalculate.length - 1) {
             
-        }
-        insert('|');
-        arrayValuesDisplay.push('|');
-    }
+    //     }
+    //     insert('|');
+    //     arrayValuesDisplay.push('|');
+    // }
 
     updateDisplay();
 })
+
+let modeOnlyDigitKey = 0;
+function checkEXPMode(e) {
+    //console.log(e.target.classList[1], e.target.classList[2]);
+    if( (e.target.classList[1] != 'btn-num' &&
+            e.target.classList[1] != 'btn-dot' &&
+            e.target.classList[1] != 'btn-del') &&
+            e.target.classList[2] != 'btn-sign') {
+        if (modeOnlyDigitKey == 1) {
+            console.log('entered EXP mode check?');
+            arrayValuesCalculate.push('*1)');
+            arrayValuesDisplay.push("</sup>");
+            modeOnlyDigitKey = 0;
+        };
+    }
+}
 
 function updateDisplay() {
     if (arrayValuesCalculate[arrayValuesCalculate.length-2] == '*' && arrayValuesCalculate[arrayValuesCalculate.length-1] == '*') {
@@ -125,21 +142,25 @@ function updateDisplay() {
     }
 }
 
-const availableChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', '-', '(', ')', '.', '|', '^'];
+const availableChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '/', '-', '(', ')', '|', '^'];
 window.addEventListener('keydown', (e) => {
     e.preventDefault();
-    console.log(e.key);
+    //console.log('type of is :' + typeof (e.key*1), e);
     if (e.key == "Escape") {document.querySelector("#btn-AC").click();}
     else if (e.key == "Enter") {document.querySelector("#btn-equal").click();} 
     else if (e.key == 'Backspace') {document.querySelector("#Del").click();}
-    else if (e.key == '*') {
-        arrayValuesCalculate.push('*');
-        insert('x');
-        arrayValuesDisplay.push('x')}
+    else if (e.key == '*') { document.querySelector('.btn-mult').click();}
+        // arrayValuesCalculate.push('*');
+        // arrayValuesDisplay.push('x')}
     else if (availableChars.indexOf(e.key) > -1) {
-        arrayValuesCalculate.push(e.key);
-        insert(e.key);
-        arrayValuesDisplay.push(e.key);
+
+        if (availableChars.indexOf(e.key) <= 9) {
+            arrayValuesCalculate.push(e.key*1);
+            arrayValuesDisplay.push(e.key*1);
+        } else {
+            arrayValuesCalculate.push(e.key);
+            arrayValuesDisplay.push(e.key);
+        }
     }
     updateDisplay();
 })
@@ -165,18 +186,26 @@ document.querySelector("#clear-mem").addEventListener('click', (e) => {
 
 // Implement backspace button functionality:
 document.querySelector("#Del").addEventListener('click', (e) => { 
-    // let content = document.querySelector("#display").textContent;
-    // inputVal = content.substring(0,content.length - 1);
-    // document.querySelector("#display").innerHTML = "";
-    // insert(inputVal);
+    
+    if (arrayValuesDisplay[arrayValuesDisplay.length - 1] == '</sup>') {
+        arrayValuesCalculate.pop();
+        arrayValuesDisplay.pop();
+        modeOnlyDigitKey = 1;
+    }
+    if (arrayValuesDisplay[arrayValuesDisplay.length - 1] == 'x10<sup>') {
+        modeOnlyDigitKey = 0;
+    }
+
     arrayValuesCalculate.pop();
     arrayValuesDisplay.pop();
+    
 })
 
 let expression = "";
 let result = 0;
 let found = "";
 document.querySelector("#btn-equal").addEventListener('click', (e) => {
+    checkEXPMode(e);
     // expression = document.querySelector("#display").innerText; //Select the current math expression.
     expression = arrayValuesCalculate.join('');
     
@@ -193,6 +222,7 @@ document.querySelector("#btn-equal").addEventListener('click', (e) => {
     expression = countParenthesesAndFix(expression);
 
     
+    expression = expression.replace(/\s/g, '');
     console.log(expression);
 
     calculate(expression); //Eval the math expression.
@@ -204,6 +234,7 @@ document.querySelector("#btn-equal").addEventListener('click', (e) => {
 function calculate(expression) {
     try {
         result = eval(expression);
+        ans = result;
         return result
     }
     catch(err) {
@@ -216,7 +247,9 @@ function calculate(expression) {
 let expHistoryArray = [];
 let resHistoryArray = [];
 let arrayValuesCalculateHistory = [];
+let arrayValuesDisplayHistory = [];
 let countHistory = 0;
+let historyString = "";
 function saveToHistorial() {
     // let equation = document.querySelector("#display").innerText;
     let equation = arrayValuesDisplay.join('');
@@ -227,9 +260,15 @@ function saveToHistorial() {
         // document.querySelector("#historial").insertAdjacentHTML("afterbegin", `${equation}${resultH} <br>`);
         document.querySelector("#historial").insertAdjacentHTML("afterbegin", `<section class='hist ${countHistory}'> ${countHistory+1}) ${equation} = ${resultH} </section>`);
 
+        historyString = JSON.stringify(arrayValuesDisplay);
+        //console.log(historyString);
+        arrayValuesDisplayHistory.push(historyString);
+        arrayValuesCalculateHistory.push(JSON.stringify(arrayValuesCalculate));
+
+
         expHistoryArray.push(equation);
         resHistoryArray.push(resultH);
-        arrayValuesCalculateHistory.push(expression);
+        //arrayValuesCalculateHistory.push(expression);
         countHistory++;
     }
 }
@@ -266,23 +305,49 @@ document.querySelector('body').addEventListener('click', function (e) {
         //console.log(e.target.classList[1]);
         let historyValue = e.target.classList[1]*1;
         //console.log(historyValue);
+        // let memResult = resHistoryArray[historyValue];
+        // let memExpr = expHistoryArray[historyValue];
+        // let memEval = arrayValuesCalculateHistory[historyValue];
+
         let memResult = resHistoryArray[historyValue];
-        let memExpr = expHistoryArray[historyValue];
-        let memEval = arrayValuesCalculateHistory[historyValue];
-        arrayValuesCalculate = [];
-        arrayValuesCalculate.push(memEval);
+        let memExpr = JSON.parse(arrayValuesDisplayHistory[historyValue]);
+        let memEval = JSON.parse(arrayValuesCalculateHistory[historyValue]);
+
+        arrayValuesCalculate = memEval;
+        // arrayValuesCalculate.push(memEval);
         insertResult(memResult);
         document.querySelector("#display").innerHTML = "";
         //console.log(memExpr);
-        arrayValuesDisplay = [];
-        arrayValuesDisplay.push(memExpr);
+        arrayValuesDisplay = memExpr;
+        // arrayValuesDisplay.push(memExpr);
         updateDisplay();
     }
 
 })
 
-function name(params) {
-    
+//Change sign functionality:
+function changeSign() {
+    let index = arrayValuesCalculate.length - 1;
+    console.log(typeof (arrayValuesCalculate[index]));
+    // if ((arrayValuesCalculate[index] == '.') || (typeof ((arrayValuesCalculate[index])) == "number")) {
+        
+    //     console.log((arrayValuesCalculate[index]));
+    //     //index--;
+        while ((arrayValuesCalculate[index] == '.') || (typeof ((arrayValuesCalculate[index])) == "number")) {
+            index--;
+        }
+        if (arrayValuesCalculate[index] != '-' && arrayValuesCalculate[index] != '+') {
+            arrayValuesCalculate.splice(index+1,0,'-');
+            arrayValuesDisplay.splice(index+1,0,'-');
+        } else if (arrayValuesCalculate[index] == '-') {
+            arrayValuesCalculate[index] = '+';
+            arrayValuesDisplay[index] = '+';
+        } else if (arrayValuesCalculate[index] == '+') {
+            arrayValuesCalculate[index] = '-';
+            arrayValuesDisplay[index] = '-';
+        }
+    // }
+    updateDisplay();
 }
 
 function countParenthesesAndFix(expression) {
